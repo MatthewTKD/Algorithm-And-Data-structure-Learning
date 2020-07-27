538761c0f02e195918e7eaafdefde7f867e2d353
Yunxi Jiang <Matthe_TKD@126.com>
2020-07-27 18:05:07 +0800

    Add the remove find and contains method

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -39,7 +39,7 @@ public class Array {
         }
 
         if (index < 0 || index > size) {
-            throw new IllegalArgumentException("Add faild. Require index >=0 and index <= " +
+            throw new IllegalArgumentException("Add failed. Require index >=0 and index <= " +
                     "size");
         }
 
@@ -54,7 +54,7 @@ public class Array {
     //Query the element at the index position
     public int get(int index) {
         if (index < 0 || index > size) {
-            throw new IllegalArgumentException("Add faild. Require index >=0 and index <= " +
+            throw new IllegalArgumentException("Get failed. Require index >=0 and index <= " +
                     "size");
         }
 
@@ -64,10 +64,62 @@ public class Array {
     //Change the element at the index position
     public void set(int index, int e) {
         if (index < 0 || index > size) {
-            throw new IllegalArgumentException("Add faild. Require index >=0 and index <= " +
+            throw new IllegalArgumentException("Set failed. Require index >=0 and index <= " +
                     "size");
         }
         data[index] = e;
+    }
+
+    // Find weather there is element e in the array
+    public boolean contains(int e) {
+        for (int i = 0; i < size; i ++) {
+            if (data[i] == e) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Find the index of element e in the array, return -1 if not found
+    public int find(int e) {
+        for (int i = 0; i < size; i ++) {
+            if (data[i] == e){
+                return i;
+            }
+        }
+        return -1;
+    }
+    // Remove the element at index position and return it
+    public int remove(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Require index >=0 and index <= " +
+                    "size");
+        }
+
+        int ret = data[index];
+        for (int i = index + 1; i < size; i ++) {
+            data[i - 1] = data[i];
+        }
+
+        size --;
+        return ret;
+    }
+    public int removeFirst() {
+        return remove(0);
+    }
+    public int removeLast() {
+        return remove(size - 1);
+    }
+
+    // Remove the element e in the array, and return true if deleted successfully or return
+    // false
+    public boolean removeElement(int e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+            return true;
+        }
+        return false;
     }
 
     @Override
