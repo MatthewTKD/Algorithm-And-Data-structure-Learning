d5e5153e45f90cb13a7e923bdf8f4b824e057811
Matthew <Matthe_TKD@126.com>
2020-08-10 15:18:12 +0800

    push O(n) pop  O(1) 改进实现方式： 1、只初始化一个队列, push时new一个新的队列 2、改进push时的循环风格, 使用while循环

--- LeetCodeProblem/Implement Stack using Queues/src/MyStack.java
+++ LeetCodeProblem/Implement Stack using Queues/src/MyStack.java
@@ -3,58 +3,37 @@ import java.util.Queue;
 
 class MyStack {
     private Queue<Integer> queue1;
-    private Queue<Integer> queue2;
 
     /** Initialize your data structure here. */
     public MyStack() {
         queue1 = new LinkedList<>();
-        queue2 = new LinkedList<>();
     }
 
     /** Push element x onto stack. */
     public void push(int x) {
-        if (queue1.isEmpty()) {
-            queue1.add(x);
-            for (int i = 0; i < queue2.size(); i ++) {
-                int ret = queue2.remove();
-                queue1.add(ret);
-            }
-        } else {
-            queue2.add(x);
-            for (int i = 0; i < queue1.size(); i ++) {
-                int ret = queue1.remove();
-                queue2.add(ret);
-            }
+        Queue<Integer> queue2 = new LinkedList<>();
+        queue2.add(x);
+        while (!queue1.isEmpty()) {
+            queue2.add(queue1.remove());
         }
 
+        queue1 = queue2;  // 让q1指向q2
     }
 
     /** Removes the element on top of the stack and returns that element. */
     public int pop() {
-        int ret;
-        if (queue1.isEmpty()) {
-            ret = queue2.remove();
-        } else {
-            ret = queue1.remove();
-        }
-        return ret;
+        return queue1.remove();
     }
 
     /** Get the top element. */
     public int top() {
-        int ret;
-        if (queue1.isEmpty()) {
-            ret = queue2.peek();
-        } else {
-            ret = queue1.peek();
-        }
-        return ret;
+        return queue1.peek();
 
     }
 
     /** Returns whether the stack is empty. */
     public boolean empty() {
-        return queue1.isEmpty() && queue2.isEmpty();
+        return queue1.isEmpty();
     }
 }
 
