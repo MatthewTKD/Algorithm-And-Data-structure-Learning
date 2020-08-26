a109aa178249de42f07bc56d542e17f12fd65e6b
Matthew <Matthe_TKD@126.com>
2020-08-26 21:29:49 +0800

    简单初始化了LinkedList
    写了LinkedList的内部类Node

---
+++ LinkedList/src/LinkedList.java
@@ -0,0 +1,20 @@
+public class LinkedList<E> {
+
+    class Node<E> {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+    }
+}
