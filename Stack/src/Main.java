7af105829c5c1fb7fc8ad58d7a0291871427d95c
Matthew <Matthe_TKD@126.com>
2020-08-28 15:46:37 +0800

    使用LinkedList实现Stack

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<>();
+        LinkedListStack<Integer> stack = new LinkedListStack<>();
         for (int i = 0; i < 5; i ++) {
             stack.push(i);
             System.out.println(stack);
