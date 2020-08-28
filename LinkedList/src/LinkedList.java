8648388d1304b3e3e4f9995ce1b3b32280df7d33
Matthew <Matthe_TKD@126.com>
2020-08-28 14:52:20 +0800

    实现了get set contains

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,6 +1,6 @@
 public class LinkedList<E> {
 
-    class Node<E> {
+    class Node {
         public E e;
         public Node next;
 
@@ -41,7 +41,7 @@ public class LinkedList<E> {
         }
 
         Node prev = dummyHead;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < index; i++) {
             prev = prev.next;
         }
 
@@ -60,4 +60,61 @@ public class LinkedList<E> {
         add(size, e);
     }
 
+
+    public E get(int index){
+
+        if(index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Illegal index.");
+        }
+
+        Node cur = dummyHead.next;
+        for(int i = 0 ; i < index ; i ++) {
+            cur = cur.next;
+        }
+
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast(){
+        return get(size);
+    }
+
+    public void set(int index, E e) {
+
+        if(index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+        }
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i ++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        for(Node cur = dummyHead.next; cur != null; cur = cur.next) {
+            res.append(cur.e + "->");
+        }
+        res.append("null");
+
+        return res.toString();
+    }
 }
