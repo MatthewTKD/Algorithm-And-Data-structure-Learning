865304816c9737f703a35531569f16d7fb2321bb
Matthew <Matthe_TKD@126.com>
2020-09-06 17:32:48 +0800

    实现InsertionSort

---
+++ InsetionSort/src/InsertionSort.java
@@ -0,0 +1,40 @@
+public class InsertionSort<E> {
+
+    private InsertionSort() {}
+
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+        for (int i = 0; i < arr.length; i ++) {
+            for (int j = i; j - 1 >= 0; j --) {
+                if (arr[j].compareTo(arr[j - 1]) < 0) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static <E> void swap(E[] arr, int i, int j) {
+        E t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    private static <E extends Comparable<E>> boolean isSorted(E[] arr){
+
+        for(int i = 1; i < arr.length; i ++)
+            if(arr[i - 1].compareTo(arr[i]) > 0)
+                return false;
+        return true;
+    }
+
+    public static void main(String[] args){
+
+        int[] dataSize = {10000, 100000};
+        for(int n: dataSize){
+            Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
+            SortingHelper.sortTest("InsertionSort", arr);
+        }
+    }
+}
+
