4da1148fc3b156ec2f930c16737f848a2870b1ae
Matthew <Matthe_TKD@126.com>
2020-09-07 13:55:34 +0800

    完成sort2 优化merge的执行次数

--- MergeSort/src/MergeSort.java
+++ MergeSort/src/MergeSort.java
@@ -1,8 +1,7 @@
 import java.util.Arrays;
 
 public class MergeSort {
-    private MergeSort() {
-    }
+    private MergeSort() {}
 
     public static <E extends Comparable<E>> void sort(E[] arr) {
         sort(arr, 0, arr.length - 1);
@@ -17,6 +16,26 @@ public class MergeSort {
         sort(arr, l, mid);
         sort(arr, mid + 1, r);
         merge(arr, l, mid, r);
+    }
+
+    // 优化merge的执行次数
+    public static <E extends Comparable<E>> void sort2(E[] arr) {
+        sort2(arr, 0, arr.length - 1);
+    }
+
+    private static <E extends Comparable<E>> void sort2(E[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int mid = l + (r - l) / 2;  // 本来可以写成(l + r) / 2 , 但(l + r)可能造成整型溢出
+        sort2(arr, l, mid);
+        sort2(arr, mid + 1, r);
+
+        // 优化optimization
+        if (arr[mid].compareTo(arr[mid + 1]) > 0) {
+            merge(arr, l, mid, r);
+        }
     }
 
     // 合并两个有序的区间 arr[l, mid] 和 arr[mid + 1, r]
@@ -42,12 +61,23 @@ public class MergeSort {
         }
     }
 
-    public static void main(String[] args) {
-        int n = 100000;
+    public static void main(String[] args){
 
+        int n = 1000000;
+
+        System.out.println("Random Array : ");
         Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
+        Integer[] arr2 = Arrays.copyOf(arr, arr.length);
 
         SortingHelper.sortTest("MergeSort", arr);
-    }
+        SortingHelper.sortTest("MergeSort2", arr2);
 
+
+        System.out.println("\nOrdered Array : ");
+        arr = ArrayGenerator.generateOrderedArray(n);
+        arr2 = Arrays.copyOf(arr, arr.length);
+
+        SortingHelper.sortTest("MergeSort", arr);
+        SortingHelper.sortTest("MergeSort2", arr2);
+    }
 }
