9f572d5a0e874078b6f93d302eda6528ef976693
Matthew <Matthe_TKD@126.com>
2020-09-07 14:29:58 +0800

    InsertionSort小优化, 不使用swap

--- InsetionSort/src/InsertionSort.java
+++ InsetionSort/src/InsertionSort.java
@@ -4,22 +4,15 @@ public class InsertionSort<E> {
 
     public static <E extends Comparable<E>> void sort(E[] arr) {
         for (int i = 0; i < arr.length; i ++) {
-            for (int j = i; j - 1 >= 0 && arr[j].compareTo(arr[j - 1]) < 0; j --) {
-//                if (arr[j].compareTo(arr[j - 1]) < 0) {
-//                    swap(arr, j, j - 1);
-//                } else {
-//                    break;
-//                }
-                swap(arr, j, j -1);
+            E t = arr[i];
+            int j;
+            for (j = i; j - 1 >= 0 && t.compareTo(arr[j - 1]) < 0; j --) {
+                arr[j] = arr[j - 1];
             }
+            arr[j] = t;
         }
     }
 
-    public static <E> void swap(E[] arr, int i, int j) {
-        E t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-    }
 
     private static <E extends Comparable<E>> boolean isSorted(E[] arr){
 
@@ -31,7 +24,7 @@ public class InsertionSort<E> {
 
     public static void main(String[] args){
 
-        int[] dataSize = {10000, 100000};
+        int[] dataSize = {1000, 10000};
         for(int n: dataSize){
             Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
             SortingHelper.sortTest("InsertionSort", arr);
