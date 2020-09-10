983f60c5e9c38905e09e99f7b55a64b4df56b802
Matthew <Matthe_TKD@126.com>
2020-09-10 14:43:33 +0800

    解决剑指 Offer 51. 数组中的逆序对

---
+++ LeetCodeProblem/剑指Offer51.数组中的逆序对LCOF/src/Solution.java
@@ -0,0 +1,53 @@
+class Solution {
+
+    private int res = 0;
+
+    public int reversePairs(int[] nums) {
+        int[] temp = new int[nums.length];
+        res = 0;
+        sort(nums, 0, nums.length - 1, temp);
+        return res;
+    }
+
+    private void sort(int[] arr, int l, int r, int[] temp) {
+        if (l >= r) {
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+        sort(arr, l, mid, temp);
+        sort(arr, mid + 1, r, temp);
+        if (arr[mid] > arr[mid + 1]) {
+            merge(arr, l, mid, r, temp);
+        }
+    }
+
+    private void merge(int[] arr, int l, int mid, int r,
+                       int[] temp) {
+        System.arraycopy(arr, l, temp, l, r - l + 1);
+
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {
+                arr[k] = temp[j];
+                j++;
+            } else if (j > r) {
+                arr[k] = temp[i];
+                i++;
+            } else if (temp[i] > temp[j]) {
+                res += mid - i + 1;
+                arr[k] = temp[j];
+                j++;
+            } else {
+                arr[k] = temp[i];
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] num = {7, 5, 6, 4};
+        solution.reversePairs(num);
+    }
+}
