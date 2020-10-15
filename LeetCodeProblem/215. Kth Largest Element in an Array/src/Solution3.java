e343ce6d9ce9204913743a50b6e34867fb3da084
Matthew <Matthe_TKD@126.com>
2020-10-15 22:30:03 +0800

    改变循环不变量重新写215号问题

--- LeetCodeProblem/215. Kth Largest Element in an Array/src/Solution3.java
+++ LeetCodeProblem/215. Kth Largest Element in an Array/src/Solution3.java
@@ -1,34 +1,35 @@
 import java.util.Random;
 
-// 非递归实现
-class Solution2 {
+// 非递归实现, 改变循环不变量
+class Solution3 {
     public int findKthLargest(int[] nums, int k) {
         Random random = new Random();
         return selectK(nums, nums.length - k, random);
     }
 
     private int selectK(int[] nums, int k, Random random) {
-        int l = 0, r = nums.length - 1;
-        // 在nums[l, r]中寻找k
-        while (l <= r) {
+        int l = 0, r = nums.length;
+
+        // nums[l, r)中寻找k
+        while (l < r) {
             int mid = partition(nums, l, r, random);
-            if(mid == k) {
+            if (mid == k) {
                 return nums[mid];
             }
-            if(mid < k) {
+            if (mid < k) {
                 l = mid + 1;
-            }else {
-                r = mid - 1;
+            } else {
+                r = mid;
             }
         }
         throw new RuntimeException("No solution");
     }
 
     private int partition(int[] nums, int l, int r, Random random) {
-        int p = l + random.nextInt(r - l + 1);
+        int p = l + random.nextInt(r - l);
         swap(nums, l, p);
-        // nums[l + 1, i - 1] <= v, nums[j + 1, r] > v
-        int i = l + 1, j = r;
+        // nums[l + 1, i - 1] <= v, nums[j + 1, r) > v
+        int i = l + 1, j = r - 1;
         while (true) {
             while (i <= j && nums[i] < nums[l]) {
                 i++;
