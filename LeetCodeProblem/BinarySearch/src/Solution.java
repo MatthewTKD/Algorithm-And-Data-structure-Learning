c50b8cd3d853201e45da0a5f9087d1054a8ff0bc
Matthew <Matthe_TKD@126.com>
2020-10-15 20:23:29 +0800

    改变循环不变量重新书写BinarySearch

---
+++ LeetCodeProblem/BinarySearch/src/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length;
+        // nums[l, r)找target
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return -1;
+    }
+}
